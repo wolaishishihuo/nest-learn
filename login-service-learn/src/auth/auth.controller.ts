487c5364a298368587d0096c9884389d3b8e9144
jiazhigang <jiazhigang_888@163.com>
2024-03-30 12:59:23 +0800

    feat: 创建基础文件

---
+++ login-service-learn/src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegesterDto } from './dto/regester.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('regester')
+  regester(@Body() regesterDto: RegesterDto) {
+    return this.authService.register(RegesterDto);
+  }
+}
---
+++ login-service-learn/src/auth/auth.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { RegesterDto } from './dto/regester.dto';
+
+@Injectable()
+export class AuthService {
+  register(regesterDto: RegesterDto) {
+    return 'register';
+  }
+}
