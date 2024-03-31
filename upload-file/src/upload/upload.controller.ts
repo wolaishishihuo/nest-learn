5391647fa5fb4c336f22e2e4af65755857d675c8
jiazhigang <jiazhigang_888@163.com>
2024-03-31 09:41:20 +0800

    feat: MulterModule模块异步配置

--- upload-file/src/upload/upload.controller.ts
+++ upload-file/src/upload/upload.controller.ts
@@ -1,34 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UploadService } from './upload.service';
-import { CreateUploadDto } from './dto/create-upload.dto';
-import { UpdateUploadDto } from './dto/update-upload.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
-
   @Post()
-  create(@Body() createUploadDto: CreateUploadDto) {
-    return this.uploadService.create(createUploadDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.uploadService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.uploadService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUploadDto: UpdateUploadDto) {
-    return this.uploadService.update(+id, updateUploadDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.uploadService.remove(+id);
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 }
--- upload-file/src/upload/upload.module.ts
+++ upload-file/src/upload/upload.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Module({
+  imports: [
+    MulterModule.registerAsync({
+      useFactory: () => {
+        return {
+          storage: diskStorage({
+            // 上传文件目录
+            destination: './uploads',
+            // 文件名重命名
+            filename: (req, file, cb) => {
+              const uniqueSuffix =
+                Date.now() + '-' + Math.round(Math.random() * 1e9);
+              cb(null, uniqueSuffix + '-' + extname(file.originalname));
+            },
+          }),
+        };
+      },
+    }),
+  ],
   controllers: [UploadController],
   providers: [UploadService],
 })
